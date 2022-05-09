1aa66738b3ebc4b6c1a032fc95ca42429191649a
Jide Igun <ig.jide@gmail.com>
2022-05-09 17:11:24 +0100

    card update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-import './App.css';
-import Cont from './cont';
+import { React } from "react";
+import "./App.css";
+import Appbar from "./Appbar";
+import Cont from "./cont";
 
-
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 
 function App() {
   return (
     <div>
-      <h2>My React App is Online at Github</h2>
-      <Cont />
+      <Appbar />
+      <Box maxWidth="xl"
+        sx={{
+          flexGrow: 1,
+          display: {
+            md: "flex",
+            flexDirection: 'column',
+            alignItems: "center",
+          },
+          m: 'auto',
+        }}
+      >
+        <h2>My React App is Online at Github. </h2>
+        <Grid
+          container
+          spacing={2}
+          sx={{ margin: "auto", justifyContent: "center" }}
+        >
+          <Grid item xs={4}>
+            <Cont />
+          </Grid>
+          <Grid item xs={4}>
+            <Cont />
+          </Grid>
+          <Grid item xs={4}>
+            <Cont />
+          </Grid>
+        </Grid>
+      </Box>
     </div>
   );
 }
--- src/cont.js
+++ src/cont.js
@@ -1,38 +1,30 @@
-import img from './images/paella.jpg';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-
+import img from "./images/paella.jpg";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 function Cont() {
-    return (
-        <div>
-            <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        height="140"
-        image={img}
-        alt="green iguana"
-      />
+  return (
+    <Card sx={{ maxWidth: 375 }}>
+      <CardMedia component="img" height="140" image={img} alt="green iguana" />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           Lizard
         </Typography>
-        <Typography variant="body2" color="text.secondary">
+        <Typography variant="" color="text.secondary">
           Lizards are a widespread group of squamate reptiles, with over 6,000
           species, ranging across all continents except Antarctica
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
+        <Button size="medium" variant="contained" color="primary">Share</Button>
+        <Button size="medium" variant="outlined" color="primary">Learn More</Button>
       </CardActions>
     </Card>
-        </div>
-    )
+  );
 }
 
-export default Cont
+export default Cont;
